a48e0cf4de1f9fb1e1b6ca678d2a13b8c2b5945d
Neelabhra Roy <136609452+Aaravos04@users.noreply.github.com>
2026-09-01 17:33:43 +0530

    Update CORS origin for production deployment

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://tic-tac-toe-xo-mania.netlify.app/",
   },
 });
 
